33296e9823a1cd497ffe10b82e4c2c4d42121b36
Dale Chen <dale@leandatainc.com>
2013-04-12 13:57:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdAddressbook::Application.config.secret_token = '80dc1bb893c76daa6a4f33b51ea8f14425e5228f98f5dbe9b10352752a201628c201b5ddfb32cd4b1cc065ccde6f257777e73032a0bf170641691a4bb61482cc'
