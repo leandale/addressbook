68b34e885acae610b31ad1dc245d114aeb31ee2c
Dale Chen <dale@leandatainc.com>
2013-04-12 14:07:49 -0700

    change name to Addressbook and change to postgres

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LdAddressbook::Application.config.secret_token = 'b516661db7045cf896b0bd9692505c0a327e841fc49e6bf64542045b74821d0871f8fa6fbd7513b1367f6c66ffc6f7104f79d1462acb7deda6f72b590cc8da4d'
+Addressbook::Application.config.secret_token = 'b516661db7045cf896b0bd9692505c0a327e841fc49e6bf64542045b74821d0871f8fa6fbd7513b1367f6c66ffc6f7104f79d1462acb7deda6f72b590cc8da4d'
